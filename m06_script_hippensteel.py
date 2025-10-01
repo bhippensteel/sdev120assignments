60c2c558bdc94597d5825bdee1838a20be7f26c7
bhippensteel <bhippensteel@ivytech.edu>
2025-10-01 13:20:13 -0400

    Add files via upload

---
+++ m06_script_hippensteel.py
@@ -0,0 +1,5 @@
+c = "cats" #Sets the variable c to cats.
+d = "dogs" #Sets the variable d to dogs.
+s = "It is raining " + c +  " and " + d + "." #Combines the variables c and d into a sentence s
+
+print(s)
