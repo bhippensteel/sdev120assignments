1c1b8664d97298270ef858a75e8d2409fb967543
bhippensteel <bhippensteel@ivytech.edu>
2025-10-14 14:02:58 -0400

    Add files via upload

---
+++ m08_script_hippensteel.py
@@ -0,0 +1,12 @@
+def greater_than(x,y):
+    if x > y:
+        return True
+    else:
+        return False
+
+a = 2
+b = 3
+
+result = greater_than(a,b)
+
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
